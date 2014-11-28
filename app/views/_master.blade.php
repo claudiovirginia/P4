a15a2fb68b96d024f959ad5a8387fb9c102800e8
Claudio Bustamante <claudiovirginia@gmail.com>
2014-11-28 09:00:10 -0500

    add images, views modified

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -1,17 +1,40 @@
 <!DOCTYPE html>
 <html>
-
 <head>
-	<title>@yield('title','Welcome')</title>
-	<meta charset="UTF-8">
+
+	<title>@yield('title','Music Library')</title>
+	<meta charset="utf-8">
+	
+	<link href="//netdna.bootstrapcdn.com/bootswatch/3.1.1/flatly/bootstrap.min.css" rel="stylesheet">
 	<link rel='stylesheet' href='{{ asset('site.css') }}'>
+	
 	@yield('head')
+		
+	
 </head>
 	
 <body>
-	 <a href='/'>&larr; Home</a>
-	 <br><br><br>
-	 @yield('body')
+	 <br>
+	 
+	 
+	@if(Session::get('flash_message'))
+		<div class='flash-message'>{{ Session::get('flash_message') }}</div>
+	@endif
+
+	<nav>
+		<ul>
+		@if(Auth::check())
+			<li><a href='/logout'>Log out {{ Auth::user()->email; }}</a></li>
+		@else
+			<li><a href='/signup'>Sign up</a> or <a href='/login'>Log in</a></li>
+		@endif
+		</ul>
+	</nav>
+
+
+	<a href='/'><img class='logo' src='/images/rock_roll.jpg' alt=Music App logo'></a>
+	@yield('content')
+	@yield('/body')
 </body>
 
 </html>
--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,54 +1,12 @@
 @extends('_master')
 
 @section('title')
-	P4 Application
+	P4 - Final Project
 @stop
 
-@section('body')
-		<h1>P4 Application - Music Store</h1>
-		<div>
-		<fieldset>	
-			<p>Lorem Ipsum Generator</p>
-			<p>In publishing and graphic design, lorem ipsum is a placeholder text commonly used to demonstrate the graphic elements of a document or visual presentation.</p>
-			<p>Let's generate some text</p>
-			<p>How many paragraphs do you want to generate?<p>
-				{{ Form::open(array('url' => '/lorem', 'method' => 'GET')) }}
-					{{ Form::label('query', 'Pick a number from the list ? '); }}
-					{{ Form::selectRange('number', 1, 10); }}
-					{{ Form::submit('Generate Paragraphs'); }}
-				{{ Form::close() }}
-		</fieldset>		
-		</div>
-				
-		<div>
-		<fieldset>	
-			<p>Random User Generator</p>
-			<p>We are creating random user data for your applications. Like Lorem Ipsum, but for people.</p>
-			<p>How many people do you want to generate?<p>
-				{{ Form::open(array('url' => '/faker', 'method' => 'GET')) }}
-						{{ Form::label('value', 'Pick a number from the list ? '); }}
-						{{ Form::selectRange('number', 1, 10); }}
-						{{ Form::submit('Generate People'); }}
-				{{ Form::close() }}
-		</fieldset>
-		</div>
-					
-		<div>
-		<fieldset>	
-			<p>Add New Group</p>
-			{{ Form::open(array('url' => '/add' )) }}
-				{{ Form::label('groupName','Group Name') }}
-				{{ Form::text('groupName'); }}
+@section('head')
+		<h1>P4 - Music Store</h1>
+@stop
 
-				{{ Form::label('musicType', 'Music Type') }}
-				{{ Form::text('musicType'); }}
-		
-				{{ Form::label('groupNo', 'Group No') }}
-				{{ Form::text('groupNo'); }}
-				
-				{{ Form::submit('Add A New Group'); }}
-			{{ Form::close() }}
-
-		</fieldset>
-		</div>
+@section('content')
 @stop
