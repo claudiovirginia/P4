ce8f7248fe5096b1a3ce86f64447f10e5683c598
Claudio Bustamante <claudiovirginia@gmail.com>
2014-12-08 21:13:17 -0500

    modified routes

--- app/routes.php
+++ app/routes.php
@@ -126,3 +126,28 @@ Route::get('/trigger-error',function() {
 Route::get('/', function() {
 	return View::make('index');
 });
+
+#truncate
+Route::get('/truncate', function() {
+
+    # Clear the tables to a blank slate
+    DB::statement('SET FOREIGN_KEY_CHECKS=0'); # Disable FK constraints so that all rows can be deleted, even if there's an associated FK
+    DB::statement('TRUNCATE groups');
+    DB::statement('TRUNCATE albums');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
